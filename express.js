46e29b7bae6e121b8dcee0b32fde8f8c0d21cfb7
theshashanknanda <shashanknanda.dev@yahoo.com>
2025-06-26 17:21:47 -0400

    initial commit

---
+++ express.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 3000
+
+app.get('/', async (req, res) => {
+    const fileName = req.query.fileName;
+    const data = await fs.readFileSync(fileName, 'utf-8');
+    
+  return res.json({
+    data: data
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
